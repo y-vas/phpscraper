f9af0cf6b4b804441b84ea2403ab2df214f9e256
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-08-11 10:30:04 +0200

    basic architecture

---
+++ Src/search.php
@@ -0,0 +1,29 @@
+
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+
+
+<form class="w-50 m-auto pt-5" action="/search" method="get">
+
+  <div class="input-group rounded">
+    <input type="text" name='q' class="form-control rounded" placeholder="Query" aria-label="Search"
+    aria-describedby="search-addon" />
+  </div>
+
+  <div class="input-group rounded mt-2">
+    <input type="number" name='pages' class="form-control rounded" placeholder="Page" aria-label="Search"
+    aria-describedby="search-addon" />
+  </div>
+
+  <div class="input-group rounded mt-2">
+    <input type="submit" class="btn btn-success btn-block" value='Search'/>
+  </div>
+
+</form>
--- index.php
+++ index.php
@@ -1,52 +1,28 @@
 <?php
-use Mark\App;
 
 require 'vendor/autoload.php';
+require 'Src/Scrapper.php';
+
+use Mark\App;
 
 $api = new App('http://0.0.0.0:3000' );
 
 $api->any('/', function ( $request ){
-  session_start();
-
-  $search = 'Vasyl Yovdiy';
-
-  // instead of using a db we use a the session to make this more simpler
-  $dbarr = $_SESSION[$search] = $_SESSION[$search] ?? [];
-
-  $dom = new DOMDocument;
-  $dom->loadHTMLFile('https://www.google.es/search?q=' . urlencode($search) );
-
-  $links = $dom->getElementsByTagName( "h3" );
-
-  foreach( $links as $link ) {
-      $anchor = $link->parentNode;
-      $url    = $anchor->getAttribute('href');
-
-      // deobfucate the url given by google
-      $q = parse_url( $url )['query'];
-      parse_str( $q , $params );
-      $url = $params['q'];
-
-      // check the url how many matches has and (store or update)
-      // them in the session
-      if ( !empty( $url ) ){
-        $count = isset($dbarr[$url]) ? $dbarr[$url] : 0;
-        $count += substr_count(file_get_contents( $url ), $search );
-        $_SESSION[$search][$url] = $count;
-      }
-  }
-
-  // sort the matches
-  uasort($_SESSION[$search], function($a, $b) {
-    return $b - $a;
-  });
-
-  // return json_encode($_SESSION[$search]);
+  return file_get_contents('Src/search.php');
 });
 
-$api->get('/search', function( $requst ) {
-  $question = $_GET['q'];
+$api->get('/search', function( $request ) {
 
+  $question = $request->get()['q'] ?? null;
+  $start    = $request->get()['pages'] ?? null;
+
+  $results = Scrapper::search( $question , $start );
+
+  ob_start();
+
+  include 'Src/results.php';
+
+  return ob_get_clean();
 });
 
 $api->start();
